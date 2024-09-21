6e4082dbc02676c7978140dd2f68dc67fa98c233
Sergey Kiskin <sector1200@gmail.com>
2024-09-21 18:18:23 +0500

    Setting up product page

--- src/pages/product/ui/ProductPage.tsx
+++ src/pages/product/ui/ProductPage.tsx
@@ -1,8 +1,5 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { CurrentProduct } from "../../../widgets/current-product";
 
 export function ProductPage(): JSX.Element {
-  const { pathname } = useLocation();
-  const productId = pathname.split("/")[2];
-
-  return <p>Welcome to product {productId}</p>;
+  return <CurrentProduct />;
 }
