009b6ed2d6b75e5966b1cc96e0f1bb47b54fd2fa
Sergey Kiskin <sector1200@gmail.com>
2024-09-23 13:11:25 +0500

    Setting up handleSetProuctInCart in model

---
+++ src/widgets/current-product/model/handleSetProductInCart.ts
@@ -0,0 +1,36 @@
+import type { ProductInCart } from "../../../shared/types";
+
+export function handleSetProductInCart(
+  name: string,
+  currentColor: string,
+  currentSizeLabel: string,
+  currentPrice: string,
+  productsInCart: ProductInCart[],
+  setProductsInCart: (products: ProductInCart[]) => void
+) {
+  const productInCartObj: ProductInCart = {
+    id: [name, currentColor, currentSizeLabel].join("_"),
+    category: name,
+    color: currentColor,
+    size: currentSizeLabel,
+    price: currentPrice,
+  };
+
+  const alreadyInCart = [...productsInCart].some(
+    (product) => product.id === productInCartObj.id
+  );
+
+  if (alreadyInCart) {
+    const chagedProductsInCart = [...productsInCart].map((product) => {
+      if (product.id === productInCartObj.id) return productInCartObj;
+      return product;
+    });
+
+    setProductsInCart(chagedProductsInCart);
+  } else {
+    const bufferArr = [...productsInCart];
+
+    bufferArr.push(productInCartObj);
+    setProductsInCart(bufferArr);
+  }
+}
