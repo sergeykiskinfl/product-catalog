6d1291c69c9a69ef1127c443e6258382c2b3b495
Sergey Kiskin <sector1200@gmail.com>
2024-09-23 16:28:41 +0500

    Link fix

--- src/entities/category-item/ui/CategoryItem.tsx
+++ src/entities/category-item/ui/CategoryItem.tsx
@@ -9,15 +9,15 @@ type Props = {
 
 export function CategoryItem({ id, name, image }: Props): JSX.Element {
   return (
-    <Link as={ReachLink} to={`/product/${id}`}>
-      <Card maxW="sm" bg="gray.300">
-        <CardBody>
+    <Card maxW="sm" bg="gray.300">
+      <CardBody>
+        <Link as={ReachLink} to={`/product/${id}`}>
           <Image src={image} alt={name} borderRadius="lg" />
           <Text mt={8} fontSize="2xl">
             {name}
           </Text>
-        </CardBody>
-      </Card>
-    </Link>
+        </Link>
+      </CardBody>
+    </Card>
   );
 }
