f00631a7728c43713f0a81fd2d26f2043cad3fcd
Sergey Kiskin <sector1200@gmail.com>
2024-09-25 20:10:34 +0500

    Any numbers of photos

--- src/widgets/current-product/model/getCurrentParams.ts
+++ src/widgets/current-product/model/getCurrentParams.ts
@@ -8,7 +8,6 @@ export function getCurrentParams(
   const { name, colors } = currentProduct!;
 
   const selectedColors = colors.map((color) => color.name);
-  const selectedPhotos = ["Спереди", "Спина"];
 
   const currentColor = searchParams.get("color") ?? colors[0].name;
   const currentColorObj =
@@ -21,26 +20,26 @@ export function getCurrentParams(
     })
     .filter((item) => item !== undefined);
 
-  const currentPhotoVariant = searchParams.get("photo") ?? "Спереди";
-  let currentImage;
+  const selectedPhotos = currentColorObj["images"].map(
+    (_, index) => "Фото " + (index + 1)
+  );
 
-  if (currentPhotoVariant === "Спереди") {
-    currentImage = currentColorObj["images"][0];
-  } else {
-    currentImage = currentColorObj["images"][1];
-  }
-
+  const currentPhotoVariant = searchParams.get("photo")
+    ? Number(searchParams.get("photo")?.split(" ")[1]) - 1
+    : 0;
+  const currentImage = currentColorObj["images"][currentPhotoVariant];
+  
   const currentSizeArr = currentColorObj["sizes"] as Size[];
   const currentSizeLabelUrlParam = searchParams.get("size");
 
   const wantedSizeLabel = currentSizeArr.find(
     (sizeObj) => sizeObj["label"] === currentSizeLabelUrlParam
   )?.label;
-  
+
   const defaultSizeObj = currentColorObj["sizes"][0] as Size;
 
   const currentSizeLabel = wantedSizeLabel ?? defaultSizeObj?.["label"];
-  
+
   const currentPrice = currentColorObj["price"];
   const description = currentColorObj["description"];
 
